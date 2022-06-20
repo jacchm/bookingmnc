a65cfebf301280bfcf3db01915aeaca6344d0e8b
jacchm <jacekchmiel03@gmail.com>
2022-06-20 17:01:08 +0200

    allow all CORS

--- src/main/java/com/mnc/booking/config/WebSecurityConfig.java
+++ src/main/java/com/mnc/booking/config/WebSecurityConfig.java
@@ -69,8 +69,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
   @Bean
   CorsConfigurationSource corsConfigurationSource() {
     CorsConfiguration configuration = new CorsConfiguration();
-    configuration.setAllowedOrigins(Arrays.asList("https://example.com"));
-    configuration.setAllowedMethods(Arrays.asList("GET", "POST"));
+    configuration.setAllowedOrigins(Arrays.asList("*"));
+    configuration.setAllowedMethods(Arrays.asList("*"));
+    configuration.setAllowedHeaders(Arrays.asList("*"));
     UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
     source.registerCorsConfiguration("/**", configuration);
     return source;
