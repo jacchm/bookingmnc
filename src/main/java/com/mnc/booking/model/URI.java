bbc216d0917c2e99e7dd17310859d533251f4813
jacek <jacekchmiel03@gmail.com>
2022-09-10 11:26:22 +0200

    fix Id problem

--- src/main/java/com/mnc/booking/model/URI.java
+++ src/main/java/com/mnc/booking/model/URI.java
@@ -4,10 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Data
 @Entity(name = "uris")
@@ -16,7 +13,7 @@ import javax.persistence.Id;
 public class URI {
 
   @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @GeneratedValue(strategy = GenerationType.SEQUENCE)
   private Integer id;
   private String roomNo;
   private String uri;
