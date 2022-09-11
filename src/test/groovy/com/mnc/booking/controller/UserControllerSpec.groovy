d0bb00f924753925ba0504d8a7a9812bcb69c762
jacek <jacekchmiel03@gmail.com>
2022-09-11 21:40:55 +0200

    add user controller integration tests

---
+++ src/test/groovy/com/mnc/booking/controller/UserControllerSpec.groovy
@@ -0,0 +1,154 @@
+package com.mnc.booking.controller
+
+import com.mnc.booking.controller.dto.ErrorResponseDTO
+import com.mnc.booking.controller.dto.user.UserCreateResponseDTO
+import com.mnc.booking.controller.dto.user.UserDTO
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.boot.web.client.RestTemplateBuilder
+import org.springframework.core.ParameterizedTypeReference
+import org.springframework.http.HttpEntity
+import org.springframework.http.HttpMethod
+import org.springframework.http.HttpStatus
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory
+import org.springframework.web.client.RestTemplate
+import org.testcontainers.containers.PostgreSQLContainer
+import org.testcontainers.spock.Testcontainers
+import spock.lang.Shared
+import spock.lang.Specification
+
+@SpringBootTest(
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+        properties = [
+                "spring.datasource.url=jdbc:tc:postgresql:11-alpine://testcontainers/booking"
+        ]
+)
+@Testcontainers
+class UserControllerSpec extends Specification implements UserControllerTestData {
+
+    @Shared
+    PostgreSQLContainer postgreSQLContainer = new PostgreSQLContainer("postgres:11-alpine")
+
+    @Autowired
+    private TestRestTemplate testRestTemplate
+
+    def setup() {
+        RestTemplateBuilder restTemplateBuilder = new RestTemplateBuilder()
+        restTemplateBuilder.requestFactory(() -> new HttpComponentsClientHttpRequestFactory())
+        testRestTemplate = new TestRestTemplate(restTemplateBuilder)
+    }
+
+    def "should create a new user and return status 201"() {
+        given: 'new user to create with a new userNo'
+        def email = "testuseremail@gmail.com"
+        def username = "testuser"
+        def newUser = prepareUserDTO(email, username)
+
+        when: 'trying to create a new user'
+        def responseEntity = testRestTemplate.exchange("/users", HttpMethod.POST, new HttpEntity(newUser), new ParameterizedTypeReference<UserCreateResponseDTO>() {
+        }, Map.of())
+
+        then: 'response should return status 201 CREATED'
+        responseEntity.statusCode == HttpStatus.CREATED
+        responseEntity.getBody().getUsername() == username
+    }
+
+    def "should not create a new user and return CONFLICT status when the user with a given username already exists"() {
+        given: 'new user to create with existing userNo'
+        def existingUsername = "receptionist"
+        def email = "testuseremail@gmail.com"
+        def newUser = prepareUserDTO(email, existingUsername)
+
+        when: 'trying to create a new user'
+        def responseEntity = testRestTemplate.exchange("/users", HttpMethod.POST, new HttpEntity(newUser), new ParameterizedTypeReference<ErrorResponseDTO>() {
+        }, Map.of())
+
+        then: 'response should return status 409 CONFLICT'
+        responseEntity.statusCode == HttpStatus.CONFLICT
+        def errorResponseDTO = responseEntity.getBody()
+        errorResponseDTO.getCode() == 409
+        errorResponseDTO.getStatus() == HttpStatus.CONFLICT.getReasonPhrase()
+        errorResponseDTO.getMessage() != null
+    }
+
+    def "should retrieve a user and return status 200"() {
+        given: 'username'
+        def username = "receptionist"
+
+        when: 'trying to retrieve a user'
+        def responseEntity = testRestTemplate.exchange("/users/{username}", HttpMethod.GET, HttpEntity.EMPTY, new ParameterizedTypeReference<UserDTO>() {
+        }, Map.of("username", username))
+
+        then: 'response should return status 200 OK'
+        responseEntity.statusCode == HttpStatus.OK
+        def userResponse = responseEntity.getBody()
+        userResponse.getUsername() == username
+        userResponse.getEmail() != null
+        userResponse.getName() != null
+        userResponse.getSurname() != null
+        userResponse.getDateOfBirth() != null
+        userResponse.getAuthorities() != null
+        userResponse.getPhoneNumber() != null
+    }
+
+    def "should not retrieve a user when does not exist and return status 404 NOT FOUND"() {
+        given: 'userNo'
+        def username = "non-existing"
+
+        when: 'trying to create a new user'
+        def responseEntity = testRestTemplate.exchange("/users/{username}", HttpMethod.GET, HttpEntity.EMPTY, new ParameterizedTypeReference<ErrorResponseDTO>() {
+        }, Map.of("username", username))
+
+        then: 'response should return status 404 NOT FOUND'
+        responseEntity.statusCode == HttpStatus.NOT_FOUND
+        def userResponse = responseEntity.getBody()
+        userResponse.getCode() == 404
+        userResponse.getStatus() != null
+        userResponse.getMessage() != null
+    }
+
+    def "should update a user role and return status 204"() {
+        given: 'username'
+        def username = "user"
+        def userRolesUpdate = prepareUserRolesUpdateDTO("ROLE_RECEPTIONIST")
+
+        when: 'trying to create a new user'
+        def responseEntity = testRestTemplate.patchForObject("/users/{username}", userRolesUpdate, Void.class, Map.of("username", username))
+//        def responseEntity = testRestTemplate.exchange("/users/{username}", HttpMethod.PATCH, new HttpEntity(userRolesUpdate), new ParameterizedTypeReference<Void>() {
+//        }, Map.of("username", username))
+
+        then: 'response should return status 204 NO_CONTENT'
+        responseEntity.statusCode == HttpStatus.NO_CONTENT
+    }
+
+    def "should not update a user role and return status 404 NOT FOUND"() {
+        given: 'user update'
+        def username = "non-existing"
+        def userRolesUpdate = prepareUserRolesUpdateDTO(username)
+
+        when: 'trying to create a new user'
+        def responseEntity = testRestTemplate.exchange("/users/{username}", HttpMethod.PATCH, new HttpEntity(userRolesUpdate), new ParameterizedTypeReference<ErrorResponseDTO>() {
+        }, Map.of("username", username))
+
+        then: 'response should return status 404 NOT FOUND'
+        responseEntity.statusCode == HttpStatus.NOT_FOUND
+        def userResponse = responseEntity.getBody()
+        userResponse.getCode() == 404
+        userResponse.getStatus() == HttpStatus.NOT_FOUND.getReasonPhrase()
+        userResponse.getMessage() != null
+    }
+
+    def "should delete a user and return status 204"() {
+        given: 'user'
+        def userNo = "20"
+
+        when: 'trying to create a new user'
+        def responseEntity = testRestTemplate.exchange("/users/{id}", HttpMethod.DELETE, HttpEntity.EMPTY, new ParameterizedTypeReference<Void>() {
+        }, Map.of("id", userNo))
+
+        then: 'response should return status 204 NO_CONTENT'
+        responseEntity.statusCode == HttpStatus.NO_CONTENT
+    }
+
+}
---
+++ src/test/groovy/com/mnc/booking/controller/UserControllerTestData.groovy
@@ -0,0 +1,27 @@
+package com.mnc.booking.controller
+
+import com.mnc.booking.controller.dto.user.UserCreationDTO
+import com.mnc.booking.controller.dto.user.UserRolesUpdateDTO
+
+trait UserControllerTestData {
+
+    static UserCreationDTO prepareUserDTO(final String email, final String username) {
+        [
+                "email"      : email,
+                "username"   : username,
+                "password"   : "p@55w0rd",
+                "name"       : "Test",
+                "surname"    : "User",
+                "dateOfBirth": "1993-01-10",
+                "phoneNumber": "512983749",
+                "photoURI"   : "https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/User_icon-cp.svg/1200px-User_icon-cp.svg.png"
+        ]
+    }
+
+    static def prepareUserRolesUpdateDTO(final String roles) {
+        [
+                "roles": roles
+        ]
+    }
+
+}
